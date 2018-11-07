993fb4f86236a16ff264720ba046d0881a57e38c
Bob Underhill <roberthill@ou.edu>
2018-11-07 13:58:46 -0600

    moved counter

--- OneDrive - University of Oklahoma/CS 2334/Practice/CollectionsTest/src/QuickSip.java
+++ OneDrive - University of Oklahoma/CS 2334/Practice/CollectionsTest/src/QuickSip.java
@@ -1,6 +1,8 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.Timer;
 
@@ -10,6 +12,9 @@ public class QuickSip
     
     public static void main(String[] args)
     {
+        
+        JFrame frame = new JFrame();
+        JButton button = new JButton("Add Coffee");
         
         
         class DrinksCoffee implements ActionListener
@@ -29,14 +34,14 @@ public class QuickSip
                 if (gulps > 0)
                 {
                     System.out.println(gulps);
-                    --gulps;
                 }
                 else if (gulps == 0)
                 {
                     System.out.println("done");
+                    JOptionPane.showMessageDialog(null,  "Make another?");
                     System.exit(0);
                 }
-                
+                --gulps;
             }
             
         }
@@ -45,7 +50,7 @@ public class QuickSip
         Timer t = new Timer (100, dc);
         t.start();
         
-        JOptionPane.showMessageDialog(null,  "Make another?");
+        
         System.exit(0);
         
     }
